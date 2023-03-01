5b6d9036494d6f2b344ad2d8429e6442e88f15ee
Maycol Espinoza Ponce <maycol.ponce@datagrove.com>
2023-03-01 13:54:38 -0600

    Important Changes and Test of JS

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,40 @@ import { render } from 'solid-js/web'
 import { A, Routes, Route, Router } from "@solidjs/router"
 import { Editor } from './editor'
 
+import { JSX } from 'solid-js';
+import { saveAs } from 'file-saver';
 
 
+//---------------------------------------------------FUNTION OF THE DOWNLOAD BOTTON-------------------------------------------------------------------
+const downloadBtn = document.getElementById('download-btn') as HTMLButtonElement;
+
+downloadBtn.addEventListener('click', () => {
+    // Código para descargar el archivo
+    const contenido = 'Este es el contenido que deseas descargar';
+    const blob = new Blob([contenido], { type: 'text/plain' });
+
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+
+    a.download = 'my-cv-file.txt';
+
+    a.dispatchEvent(new MouseEvent('click'));
+});
+//--------------------------------------------------END OF THE FUNCION DOWNDLOAD BOTTON-------------------------------------------------------------------
 
 
+const button = document.querySelector('button[aria-controls="mobile-menu"]');
+const menu = document.querySelector('#mobile-menu');
 
-                    const button = document.querySelector('button_menu[aria-controls="mobile-menu"]');
-                    const menu = document.querySelector('#mobile-menu');
-                  
-                    button.addEventListener('click', () => {
-                      // Cambia el icono del botón
-                      button.querySelector('svg:first-child').classList.toggle('hidden');
-                      button.querySelector('svg:last-child').classList.toggle('hidden');
-                  
-                      menu.classList.toggle('hidden');
-                      menu.classList.toggle('block');
-                    });
-               
+    button.addEventListener('click', () => {
+    // Cambia el icono del botón
+    button.querySelector('svg:first-child').classList.toggle('hidden');
+    button.querySelector('svg:last-child').classList.toggle('hidden');
+
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('block');
+});
+
+
+  
